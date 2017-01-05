92552ba7949958bf80e1ce28356cb84b24fdcf5c
Paul Hunt <woodburysoft@gmail.com>
2017-01-05 16:02:45 +0000

    Imported existing code

---
+++ src/CloneRanger.Tests/test/IntPropertyClass.cs
@@ -0,0 +1,7 @@
+namespace CloneRanger.Tests.test
+{
+    internal class IntPropertyClass
+    {
+        internal int IntProperty { get; set; }
+    }
+}
---
+++ src/CloneRanger/Cloner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace CloneRanger
+{
+    public class Cloner
+    {
+        public T Clone<T>(T objectToClone) where T: class
+        {
+            return CloneObject(objectToClone);            
+        }
+
+        private T CloneObject<T>(T objectToClone) where T: class
+        {
+            if (objectToClone == null)
+            {
+                return default(T);
+            }
+
+            var clone = Activator.CreateInstance<T>();
+
+            foreach (PropertyInfo property in objectToClone.GetType().GetRuntimeProperties())
+            {
+                if (property.PropertyType.GetTypeInfo().IsPrimitive 
+                    || property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) 
+                    || property.PropertyType == typeof(string))
+                {
+                    property.SetValue(clone, property.GetValue(objectToClone));
+                }
+                else
+                {                    
+                    MethodInfo method = GetType().GetTypeInfo().GetDeclaredMethod(nameof(CloneObject)).MakeGenericMethod(property.PropertyType.IsConstructedGenericType ? property.PropertyType.GenericTypeArguments[0] : property.PropertyType);
+                    property.SetValue(clone, method.Invoke(this, new[] { property.GetValue(objectToClone) }));                                                       
+                }
+            }
+
+            return clone;
+        }        
+    }
+}
