2c41163209d63fd876f60eea88fa8b069f02f4ba
Paul Hunt <woodburysoft@gmail.com>
2017-01-05 16:12:04 +0000

    Removed unused dependency and refactored Cloner class.

--- src/CloneRanger/Cloner.cs
+++ src/CloneRanger/Cloner.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
 
 namespace CloneRanger
 {
     public class Cloner
     {
         public T Clone<T>(T objectToClone) where T: class
-        {
-            return CloneObject(objectToClone);            
-        }
-
-        private T CloneObject<T>(T objectToClone) where T: class
         {
             if (objectToClone == null)
             {
@@ -23,16 +16,16 @@ namespace CloneRanger
 
             foreach (PropertyInfo property in objectToClone.GetType().GetRuntimeProperties())
             {
-                if (property.PropertyType.GetTypeInfo().IsPrimitive 
-                    || property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) 
+                if (property.PropertyType.GetTypeInfo().IsPrimitive
+                    || property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)
                     || property.PropertyType == typeof(string))
                 {
                     property.SetValue(clone, property.GetValue(objectToClone));
                 }
                 else
-                {                    
-                    MethodInfo method = GetType().GetTypeInfo().GetDeclaredMethod(nameof(CloneObject)).MakeGenericMethod(property.PropertyType.IsConstructedGenericType ? property.PropertyType.GenericTypeArguments[0] : property.PropertyType);
-                    property.SetValue(clone, method.Invoke(this, new[] { property.GetValue(objectToClone) }));                                                       
+                {
+                    MethodInfo method = GetType().GetTypeInfo().GetDeclaredMethod(nameof(Clone)).MakeGenericMethod(property.PropertyType.IsConstructedGenericType ? property.PropertyType.GenericTypeArguments[0] : property.PropertyType);
+                    property.SetValue(clone, method.Invoke(this, new[] { property.GetValue(objectToClone) }));
                 }
             }
 
